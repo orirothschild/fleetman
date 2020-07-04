f70ebb6497869035da9b3bd67ea4c2338ab33e60
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-04 18:34:14 +0000

    completed part 1

--- start_env.sh
+++ start_env.sh
@@ -3,10 +3,10 @@
 kops edit cluster --name ${NAME}
 #add these authenticationTokenWebhook: true    authorizationMode: Webhook
 kops update cluster ${NAME} --yes
-sleep 8m
+sleep 6m
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
+kubectl apply -f .
 kubectl delete secret alertmanager-monitoring-prometheus-oper-alertmanager
 helm install monitoring stable/prometheus-operator
 kubectl delete secret alertmanager-monitoring-prometheus-oper-alertmanager
 kubectl create secret generic alertmanager-monitoring-prometheus-oper-alertmanager --from-file ../secrets/alertmanager.yaml
-kubectl apply -f .
