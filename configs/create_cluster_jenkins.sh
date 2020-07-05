08bddba3d0526d0e83edd2556d023fc20ed427a1
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-05 14:23:00 +0000

    premote to jenkins

---
+++ configs/create_cluster_jenkins.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kops create cluster --zones us-east-1a,us-east-1b,us-east-1c,us-east-1d,us-east-1e,us-east-1f ${NAME}
--- configs/start_env.sh
+++ configs/start_env.sh
@@ -8,5 +8,6 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 kubectl apply -f .
 kubectl delete secret alertmanager-monitoring-prometheus-oper-alertmanager
 helm install monitoring stable/prometheus-operator
+helm install spinnaker  stable/spinnaker --version 2.0.0-rc9
 kubectl delete secret alertmanager-monitoring-prometheus-oper-alertmanager
 kubectl create secret generic alertmanager-monitoring-prometheus-oper-alertmanager --from-file ../secrets/alertmanager.yaml
