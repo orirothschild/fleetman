97ec91c0cf23ceffa765eda4e488206dab462e09
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-12 21:15:51 +0000

    ingress but not working

--- configs/step2_start_env.sh
+++ configs/step2_start_env.sh
@@ -6,6 +6,7 @@ kops update cluster ${NAME} --yes
 sleep 6m
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 kubectl apply -f ../aws_storage/storage-aws.yaml
+helm install nginx-ingress stable/nginx-ingress --set controller.publishService.enabled=true
 helm install monitoring stable/prometheus-operator
 kubectl delete secret alertmanager-monitoring-prometheus-oper-alertmanager
-kubectl create secret generic alertmanager-monitoring-prometheus-oper-alertmanager --from-file ../secrets/alertmanager.yaml
+kubectl create secret generic alertmanager-monitoring-prometheus-oper-alertmanager --from-file ../../secrets/alertmanager.yaml
